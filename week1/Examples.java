c94bc2852167871ec239a38c657cd61894cc254a
matthouseworth <housewom@go.stockton.edu>
2022-09-22 19:24:32 -0400

    test

---
+++ week1/Examples.java
@@ -0,0 +1,42 @@
+package week1;
+
+public class Examples {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.println("Black");
+        //Java does math
+        System.out.println(3+3);// addition
+        System.out.println(2+3);// subtraction
+        System.out.println(3*3);// multiplication
+        System.out.println(3/3);// division
+        System.out.println(3%3);// modulus - remainder of division
+
+        // Java Types - 8 primitive types
+        // int - integer - whole numbers 3, 4, 5
+        // double - decimal numbers - 3.0, 4.0, 5.7
+        // char - character - single letter 'a', 'b', 'c'
+        // boolean - true or false
+        // String - a sequence of characters - "hello", "world", "Greg"
+        // long - integer 
+        // float - decimal
+        // short - integer
+
+        // Scope Types
+        // Local - inside a method
+        // Instance - inside a class
+        // Class - inside a class, but outside a method
+        // Public - anywhere in the program
+        // Private - only inside the class
+        // Protected - inside the class and subclasses
+        // default - inside the package
+
+        // Java Operators
+        // + - * / % ++ -- += -= *= /= %= == != > < >_ _< && || !
+        // = - assignment operator
+        // == - equality operator
+        // !== - not equal operator
+
+        //String Concatenation
+        System.out.println("Hello" + " " + "World");
+    }
+}
